9f874a0f9b5a55605ee8a85c4b2415bf98fca81b
ntalbs <ntalbsen@gmail.com>
2022-05-22 10:38:23 +0100

    First commit: implement echo server

---
+++ app.js
@@ -0,0 +1,51 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+function queries(url) {
+  let queries = {};
+  for (var [k, v] of url.searchParams.entries()) {
+    if (!queries[k]) {
+      queries[k] = v;
+    } else if (Array.isArray(queries[k])) {
+      queries[k].push(v);
+    } else {
+      queries[k] = [queries[k], v];
+    }
+  }
+  return queries;
+}
+
+async function body(req) {
+  const buffers = [];
+  for await (const chunk of req) {
+    buffers.push(chunk);
+  }
+  const data = Buffer.concat(buffers).toString();
+  try {
+    return JSON.parse(data);
+  } catch (e) {
+    return null
+  }
+}
+
+const server = http.createServer(async (req, res) => {
+  const url = new URL(req.url, `http://${hostname}:${port}`);
+  console.info(new Date(), req.method, req.url);
+  const echo = {
+    "method": req.method,
+    "path": url.pathname,
+    "queries": queries(url),
+    "headers": req.headers,
+    "body": await body(req)
+  };
+
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify(echo));
+});
+
+server.listen(port, () => {
+  console.log(`Server running at http://${hostname}:${port}`);
+});
